77c24280bbf7068a3a6739c9707e874c71c64bd7
z125517-LucaKeanu-Fridrich <z125517@shibaura-it.ac.jp>
2025-12-09 18:58:13 +0900

    Implement array sorting in ahw9-2.c
    Add a program to read and sort an array of integers

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int a[5], i, j;
+
+    for (i = 0; i < 5; i++) scanf("%d", &a[i]);
+
+    for (i = 0; i < 5; i++)
+        for (j = i + 1; j < 5; j++)
+            if (a[j] < a[i]) { int t = a[i]; a[i] = a[j]; a[j] = t; }
+
+    for (i = 0; i < 5; i++) {
+        if (i) printf(" ");
+        printf("%d", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
 
