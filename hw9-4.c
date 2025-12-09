f7d874e962af79813604d09682b5c69c16b66637
z125517-LucaKeanu-Fridrich <z125517@shibaura-it.ac.jp>
2025-12-09 18:56:44 +0900

    Implement RGB input and hex code display
    Added RGB input and hex code output functionality.

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int r, g, b;
+
+    printf("Enter R, G, B value\n");
+    scanf("%d", &r);
+    scanf("%d", &g);
+    scanf("%d", &b);
+
+    printf("The hex code is #%02X%02X%02X\n", r, g, b);
+    return 0;
+}
 
